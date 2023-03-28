252daf240893fcecca399bbc56d282bd1848d7b9
marcomazzotti <marcoalbertomazzotti@gmail.com>
2023-03-28 15:43:28 +0200

    Add: bundle 2 completed

--- bundle-2/main.js
+++ bundle-2/main.js
@@ -15,6 +15,12 @@ for (let i = 0; i > 5; i++) {
     console.log(i);
 }
 
+//CORREZIONE ESERCIZIO 1
+// 1. Ciclo for da 0 a 4
+// 2. Nel ciclo for i deve essere minore di 5
+// 3. No errori logici
+
+
 
 // ESERCIZIO 2
 function addIfEven(num) {
@@ -24,6 +30,12 @@ function addIfEven(num) {
     return num;
 }
 
+//CORREZIONE ESERCIZIO 2
+// 1. Funzione che aggiunge 5 al numero se è pari
+// 2. Condizione impostata come assegnazione e non come ugualianza 
+// 3. No errori logici
+
+
 
 // ESERCIZIO 3
 function loopToFive() {
@@ -31,6 +43,12 @@ function loopToFive() {
         console.log(i);
     }
 }
+
+//CORREZIONE ESERCIZIO 3
+// 1. Funzione che crea ciclo for per 5 volte e stampa i in console
+// 2. Nel ciclo for va usato ; non ,
+// 3. No errori logici
+
 
 
 // ESERCIZIO 4 (suggerimento: ci sono 8 errori)
@@ -45,3 +63,15 @@ function displayEvenNumbers() {
     }
 }
 displayEvenNumbers(); // [2,4,6,8]
+
+//CORREZIONE ESERCIZIO 4
+// 1. Dato un array, ne crea un altro con numeri pari
+// 2.   -  ; di troppo nel ciclo for
+//      -  === mancante nell'if
+//      -  mancante numbers[i]
+//      - va pushato numbers[i] non i
+//      - return fuori dal ciclo for
+//      - array dovrebbero essere const 
+//      - non va il ; dopo if
+//      - in if non va lenght - 1
+//3. No errori logici
